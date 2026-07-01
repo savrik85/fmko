85befbddbecccc3c980e92e560dd8daa1a20f11f
Zdeněk Jungvirt <zdenek.jungvirt@rohlik.cz>
2026-07-01 21:20:33 +0200

    fix(sponsors): jednoslovné názvy top sponzorů Prachatice (Dřevona, Roklan, Kovex…)

--- apps/api/migrations/0108_top_sponsors_prachatice.sql
+++ apps/api/migrations/0108_top_sponsors_prachatice.sql
@@ -1,11 +1,12 @@
+-- 10 nových jednoslovných top sponzorů do okresu Prachatice (víc variety na špičce).
 INSERT INTO district_sponsors (district, name, type, monthly_min, monthly_max, win_bonus_min, win_bonus_max) VALUES
-('Prachatice','Šumavské pily','firma',5500,9000,900,1600),
-('Prachatice','Lesy Boubín','firma',5000,8200,800,1400),
-('Prachatice','Pivovar Prachatice','obchod',5200,8500,850,1500),
-('Prachatice','Sklárny Lenora','firma',4800,7800,700,1300),
-('Prachatice','Auto Šumava','obchod',5000,8000,750,1350),
-('Prachatice','Stavebniny Vimperk','firma',4600,7200,650,1150),
-('Prachatice','Mlékárna Volary','farma',4500,7000,600,1050),
-('Prachatice','Elektro Zdíkov','firma',4300,6600,550,1000),
-('Prachatice','Wood-Tec Husinec','firma',5000,8300,800,1450),
-('Prachatice','Šumava Energo','firma',5300,8800,850,1550);
+('Prachatice','Dřevona','firma',5500,9000,900,1600),
+('Prachatice','Šumavan','obchod',5300,8800,850,1550),
+('Prachatice','Roklan','firma',5200,8500,850,1500),
+('Prachatice','Kovex','firma',5000,8300,800,1450),
+('Prachatice','Fürst','firma',5000,8200,800,1400),
+('Prachatice','Stavex','firma',5000,8000,750,1350),
+('Prachatice','Elmont','firma',4800,7800,700,1300),
+('Prachatice','Kubeš','obchod',4600,7200,650,1150),
+('Prachatice','Agrona','farma',4500,7000,600,1050),
+('Prachatice','Termex','firma',4300,6600,550,1000);
